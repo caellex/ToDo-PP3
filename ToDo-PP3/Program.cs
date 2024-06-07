e522b6a54e258b671637a7d34cb4028aea983786
caellex <winthercarlfredrik@gmail.com>
2024-06-07 11:43:42 +0200

    test

--- ToDo-PP3/Program.cs
+++ ToDo-PP3/Program.cs
@@ -6,16 +6,18 @@ class Program
 {
     static void Main(string[] args)
     {
-
         ToDoProgram program = new ToDoProgram();
         ToDoProgram.LoadTasks();
 
         while (true)
         {
             Console.Clear();
-            Console.WriteLine("Meny for To Do-app:");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Menu for To Do-app:");
+            Console.ResetColor();
             Console.WriteLine("1. Create new task");
             Console.WriteLine("2. Show tasks");
+            Console.WriteLine("3. Exit program");
 
             var userInput = Console.ReadLine();
 
@@ -27,10 +29,17 @@ class Program
                 case "2":
                     program.ShowTasks();
                     break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Invalid input. Please enter in 1, 2 or 3");
+                    Console.ResetColor();
+                    Thread.Sleep(1000);
+                    break;
             }
-
         }
     }
-
-
 }
--- ToDo-PP3/ToDoProgram.cs
+++ ToDo-PP3/ToDoProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,7 +21,9 @@ namespace ToDo_PP3
             string taskDescInput = Console.ReadLine();
 
             ToDoProgram.Tasks.Add(new ToDo(taskNameInput, taskDescInput));
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Task: {taskNameInput} added!");
+            Console.ResetColor();
             Thread.Sleep(2000);
         }
 
@@ -28,45 +31,107 @@ namespace ToDo_PP3
         {
             Console.Clear();
             int i = 0;
+            if (Tasks.Count > 0)
+            {
+                PrintAllTasks(i);
+
+                SpecificTaskMenu();
+            }
+            else
+            {
+                FinishedTasks();
+            }
+        }
+
+        private void PrintAllTasks(int i)
+        {
             foreach (ToDo task in Tasks)
             {
                 Console.WriteLine($"Task Name: {task.Task}");
-                Console.WriteLine($"Task Description: {task.Description}");
-                Console.WriteLine($"ID: {i}\n");
+                Console.WriteLine($"ID: {i + 1}\n");
                 i++;
             }
-            
-            
+        }
 
-            Console.WriteLine("Type out any ID to remove the specific task, or press any key to go back..");
+        private void SpecificTaskMenu()
+        {
+            Console.WriteLine("Choose the ID of the task you want to show");
             char userChoice = Console.ReadKey().KeyChar;
 
 
             if (Int32.TryParse(userChoice.ToString(), out int indexToRemove))
             {
-                if(indexToRemove <= Tasks.Count - 1){
-                Console.WriteLine($"Task '{Tasks[indexToRemove].Task}' removed successfully!");
-                Tasks.RemoveAt(indexToRemove);
-                Thread.Sleep(3000);
+                if (indexToRemove - 1 <= Tasks.Count - 1)
+                {
+                    Console.WriteLine("Choose the ID of the task you want to show");
+                    ShowSpecificTask(indexToRemove - 1);
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Please choose an existing task!");
+                    Console.ResetColor();
                     Thread.Sleep(1500);
                 }
+            }
+        }
+
+        private void FinishedTasks()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You have finished all your tasks! Go get a beer.");
+            Console.ResetColor();
+            Thread.Sleep(2000);
+        }
+
+        private void ShowSpecificTask(int index)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"\nName of task: ");
+            Console.ResetColor();
+            Console.Write($"{Tasks[index].Task}\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"Description of task: ");
+            Console.ResetColor();
+            Console.Write($"{Tasks[index].Description}\n\n");
+            
+            TaskMenu(index);
+        }
+
+        private void TaskMenu(int index)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("What do you want to do with this task?");
+            Console.ResetColor();
+            Console.WriteLine("1. Remove it from the list");
+            Console.WriteLine("2. Go back to main menu");
+
+            var userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{Tasks[index].Task} is now successfully deleted!");
+                    Console.ResetColor();
+                    Tasks.RemoveAt(index);
+                    Thread.Sleep(2000);
+                    break;
+                case "2":
+                    break;
             }
         }
 
         public static void LoadTasks()
         {
             ToDo task1 = new ToDo("wash windows", "washing kitchen windows");
+            ToDo task2 = new ToDo("wash living room", "wash floors, tv and sofa");
+            ToDo task3 = new ToDo("clean kitchen", "Do dishes, clean fridge, wash windows");
             Tasks.Add(task1);
+            Tasks.Add(task2);
+            Tasks.Add(task3);
         }
     }
-
-
 }
 
-// Fjerne beskrivelse fra Show Tasks -  Legge til beskrivelse når du går inn i en task istedenfor
-// Trekke ut ting i flere metoder
-// Se på en annen måte å slette tasks på (Kanskje du kan velge å slette når du går inn? (Følger indexToRemove)
